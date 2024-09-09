f7bc29a0af975472c394299178d404b1595c2218
Emerson Patryck da Silva <emeps@outlook.com>
2024-09-09 18:50:35 -0300

    chore: add save token in cookies

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+      <RouterProvider router={router}/>
   </StrictMode>,
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,10 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import axios from "axios";
+import Cookies from 'js-cookie'
 
-import logo from "../assets/image/logo.png"
+import logo from "../assets/image/logo.png";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -16,50 +18,28 @@ import {
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useNavigate } from "react-router-dom";
 
 const formSchema = z.object({
   email: z
     .string()
-    .min(2, {
-      message: "Email precisa ter mais que 2 caracteres",
-    })
-    .max(50, {
-      message: "Email precisa ter menos que 50 caracteres",
-    })
-    .email({
-      message: "Email inválido",
-    }),
-  // TODO: Remover mais tarde!
-  /* .refine((email) => email.endsWith("@example.com"), {
-      message: "Email precisa ser do domínio @example.com",
-    }), */
+    .min(2, { message: "Email precisa ter mais que 2 caracteres" })
+    .max(50, { message: "Email precisa ter menos que 50 caracteres" })
+    .email({ message: "Email inválido" }),
   password: z
     .string()
-    .min(8, {
-      message: "Senha precisa ter pelo menos 8 caracteres",
-    })
-    .max(100, {
-      message: "Senha precisa ter menos que 100 caracteres",
-    })
-    .regex(/[A-Z]/, {
-      message: "Senha precisa ter pelo menos uma letra maiúscula",
-    })
-    .regex(/[a-z]/, {
-      message: "Senha precisa ter pelo menos uma letra minúscula",
-    })
-    .regex(/[0-9]/, {
-      message: "Senha precisa ter pelo menos um número",
-    })
-    .regex(/[^A-Za-z0-9]/, {
-      message: "Senha precisa ter pelo menos um caractere especial",
-    }),
+    .min(8, { message: "Senha precisa ter pelo menos 8 caracteres" })
+    .max(100, { message: "Senha precisa ter menos que 100 caracteres" })
+    .regex(/[A-Z]/, { message: "Senha precisa ter pelo menos uma letra maiúscula" })
+    .regex(/[a-z]/, { message: "Senha precisa ter pelo menos uma letra minúscula" })
+    .regex(/[0-9]/, { message: "Senha precisa ter pelo menos um número" })
+    .regex(/[^A-Za-z0-9]/, { message: "Senha precisa ter pelo menos um caractere especial" }),
 });
 
 export function Login() {
@@ -71,17 +51,32 @@ export function Login() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // TODO: Falta finalizar a validação do login e redirecionar o usuario para pagina principal
-    console.log(values);
-  }
+  const navigate = useNavigate()
 
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const response = await axios.post("http://localhost:3001/auth/login", {
+        email: values.email,
+        password: values.password,
+      });
+      const {data} = response
+      if(!Cookies.get('token')){
+        Cookies.remove('token')
+      }
+      Cookies.set('token', data.token.access_token )  
+      localStorage.setItem( 'name', data.name)
+      navigate('/')
+    } catch (error) {
+      console.error("Erro de autenticação:", error.response?.data || error.message);
+    }
+  }
   return (
     <div className="flex items-center justify-center h-screen">
       <Card className="max-w-md w-full">
         <CardHeader>
-        <CardTitle className="flex flex-col justify-center items-center" >
-            <img  className="w-24 py-3" src={logo}/>
+          <CardTitle className="flex flex-col justify-center items-center">
+            <img className="w-24 py-3" src={logo} alt="logo" />
             <p className="text-center text-2xl">Entrar no Taskapp</p>
           </CardTitle>
           <CardDescription className="text-center text-base">
@@ -100,9 +95,6 @@ export function Login() {
                     <FormControl>
                       <Input placeholder="example@example.com" {...field} />
                     </FormControl>
-                    <FormDescription>
-                      Digite seu endereço de email.
-                    </FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
@@ -116,7 +108,6 @@ export function Login() {
                     <FormControl>
                       <Input type="password" placeholder="******" {...field} />
                     </FormControl>
-                    <FormDescription>Digite sua senha.</FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
